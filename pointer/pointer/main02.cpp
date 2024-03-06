efd86bbd902fe4f6d73ab6f7d0368e6946acf5d6
KimDoHyun1011 <hot99009@gmail.com>
2024-03-06 15:04:35 +0900

    포인터 재학습

--- pointer/pointer/main02.cpp
+++ pointer/pointer/main02.cpp
@@ -2,10 +2,39 @@
 
 using namespace std;
 
+struct Status
+{
+	int Hp;
+	int Mp;
+	int Def;
+	int Atk;
+	int Spd;
+	int Lck;
+};
 
+Status* info;
 
+void Player()
+{
+	info->Hp = 100;
+	info->Mp = 50;
+	info->Atk = 10;
+	info->Def = 5;
+	info->Lck = 0;
+	info->Spd = 3;
+
+	
+	if (info->Hp == 0)
+	{	while (true)
+		cout << "플레이어가 죽었습니다." << endl;
+		return;
+	}
+
+	cout << "플레이어 체력 :" << info->Hp << endl;
+	
+}
 
 int main()
 {
-
+	Player();
 }
