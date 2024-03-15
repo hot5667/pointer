226c56fce4d108d02eba11e6bc2de638f8fca178
KimDoHyun1011 <hot99009@gmail.com>
2024-03-16 03:02:43 +0900

    은닉성 학습 시작

--- pointer/pointer/Class.cpp
+++ pointer/pointer/Class.cpp
@@ -1,62 +1,74 @@
 #include <iostream>
 
-class Inventory
+//class Inventory
+//{
+//
+//};
+//
+//class Player
+//{
+//
+//public:
+//	void Move() {}
+//	void Attack() {}
+//	void Die() {}
+//
+//public:
+//	int _hp;
+//	int _attack;
+//	int _defence;
+//	Inventory _inventory
+//};
+//
+//class Knight : public Player
+//{
+//public:
+//	int _stmina;
+//
+//};
+//
+//class Archer : Player
+//{
+//
+//public:
+//	int  _arrCount;
+//};
+//
+//class Mage : public Player
+//{
+//
+//public:
+//	int _mp;
+//};
+//
+//void Fight(Player* p1, Player* p2)
+//{
+//	p1->_hp -= p2->_attack;
+//}
+
+class car 
 {
-
-};
-
-class Player
-{
-
 public:
-	void Move() {}
-	void Attack() {}
-	void Die() {}
+	void MoveHandle(){}
+	void pushPedal(){}
+	void OpenDoor(){}
 
+	void Disassemble(){}
+	void PutFuellnEngine(){}
+	void ConnectCircuit() {}
 public:
-	int _hp;
-	int _attack;
-	int _defence;
-	Inventory _inventory
-};
-
-class Knight : public Player
-{
 
 
 };
-
-class Archer : Player
-{
-
-public:
-	int  _arrCount;
-};
-
-class Mage : Player
-{
-
-public:
-	int _mp;
-};
-
-void Fight(Knight& k1, Knight& k2)
-{
-
-}
 
 int main()
 {
-	Knight k1;
-	Mage m1;
-	m1._mp;
-	
 
 }
 
 //oop 3대 요소
 // -상속성 << inheritance
-// -은닉성
+// -은닉성 << data hiding (캡슐화 encapsulation)
 // -다형성
 
 // Player 기본 클래스
