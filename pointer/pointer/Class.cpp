09b56d1c82d3187de9c1541c170abe6d3ba38e85
KimDoHyun1011 <hot99009@gmail.com>
2024-03-16 00:17:20 +0900

    Class 학습

--- pointer/pointer/Class.cpp
+++ pointer/pointer/Class.cpp
@@ -2,7 +2,43 @@
 
 int main()
 {
-
+	
 
 
 }
+
+//oop 3대 요소
+// -상속성 << inheritance
+// -은닉성
+// -다형성
+
+
+class Player
+{
+public:
+	void Move(){}
+	void Attack(){}
+	void Die(){}
+
+public:
+	int _hp;
+	int _attack;
+	int _defence;
+};
+
+class Knight : Player 
+{
+
+};
+
+class Archer : Player 
+{
+
+public:
+	int  _arrCount;
+};
+
+class Mage : Player
+{
+
+};
