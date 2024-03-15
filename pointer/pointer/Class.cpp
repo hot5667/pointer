5f110a182c4aca672ef63a413e8447be4354cbff
KimDoHyun1011 <hot99009@gmail.com>
2024-03-16 01:38:08 +0900

    상속성 Class 학습

--- pointer/pointer/Class.cpp
+++ pointer/pointer/Class.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
 
-int main()
+class Inventory
 {
-	
 
+};
 
-}
-
-//oop 3대 요소
-// -상속성 << inheritance
-// -은닉성
-// -다형성
-
-// Player 기본 클래스
 class Player
 {
 
 public:
-	void Move(){}
-	void Attack(){}
-	void Die(){}
+	void Move() {}
+	void Attack() {}
+	void Die() {}
 
 public:
 	int _hp;
 	int _attack;
 	int _defence;
-
+	Inventory _inventory
 };
 
-class Knight : Player 
+class Knight : public Player
 {
 
+
 };
 
-class Archer : Player 
+class Archer : Player
 {
 
 public:
@@ -46,3 +39,26 @@ class Mage : Player
 public:
 	int _mp;
 };
+
+void Fight(Knight& k1, Knight& k2)
+{
+
+}
+
+int main()
+{
+	Knight k1;
+	Mage m1;
+	m1._mp;
+	
+
+}
+
+//oop 3대 요소
+// -상속성 << inheritance
+// -은닉성
+// -다형성
+
+// Player 기본 클래스
+
+// ls-A vs Has-A
