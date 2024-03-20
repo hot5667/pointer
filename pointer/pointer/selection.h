2650d3cfdd2927d643c5c577afaf53228809fd6c
KimDoHyun1011 <hot99009@gmail.com>
2024-03-20 13:29:32 +0900

    알고리즘 Shorting

---
+++ pointer/pointer/selection.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "sorting.h"
+
+template <class T>
+
+class Selection : public Sort<T>
+{
+public:
+	Selection(int Max = 100) :Sort<T>(Max) {}
+	void sorting() override;
+};
+template <class T>
+void Selection<T>::sorting()
+{
+	int N = this->getSize();
+	T* data = this->getData();
+	for (int last = N - 1; last >= 1; --last) {
+		int largestIndex = 0;
+		for (int cur = 1; cur <= last; cur++) {
+			if (data[cur] > data[largestIndex])
+				largestIndex = cur;
+		}
+		swap(data[last], data[largestIndex]);
+	}
+}
