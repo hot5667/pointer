d274339ff3b38e0f9040b0dbeebe359f24b45e58
KimDoHyun1011 <hot99009@gmail.com>
2024-03-20 21:28:41 +0900

    테스트

--- pointer/pointer/CArrary.h
+++ pointer/pointer/CArrary.h
@@ -5,9 +5,14 @@ class CArrary
 
 private:
 	int* m_pData;
-	int  m_Capacity;
-	int  m_Size;
+	unsigned int  m_Capacity;
+	unsigned int  m_Size;
 
+public:
+	void PushBack(int _Data);
+	void Resize(int _Size);
+	int  GetData(int _idx);
+	int operator[] (unsigned int _idx);
 public:
 	CArrary();
 	~CArrary();
