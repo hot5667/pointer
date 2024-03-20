7d5270ce1d4d642ecbd645a651451885c98e74a1
KimDoHyun1011 <hot99009@gmail.com>
2024-03-20 14:44:20 +0900

    버블 쇼팅 구현 - 교수님 코드 리펙토링

--- pointer/pointer/bubble.h
+++ pointer/pointer/bubble.h
@@ -1,25 +1,25 @@
 #pragma once
 #include "sorting.h"
+
 template <class T>
 class Bubble : public Sort<T>
 {
 public:
-	Bubble(int Max = 100) :Sort<T>(Max) {}
-	void sorting() override;
+    Bubble(int Max = 100) : Sort<T>(Max) {}
+
+    void sorting() override {
+        int N = this->getSize();
+        const T* data = this->getData(); 
+
+        for (int pass = 1; pass < N; ++pass) {
+            bool sorted = true;
+            for (int cur = 0; cur < N - pass; ++cur) {
+                if (data[cur] > data[cur + 1]) {
+                    std::swap(const_cast<T&>(data[cur]), const_cast<T&>(data[cur + 1])); 
+                    sorted = false;
+                }
+            }
+            if (sorted) break;
+        }
+    }
 };
-template <class T>
-void Bubble<T>::sorting()
-{
-	int N = this->getSize();
-	T* data = this->getData();
-	bool sorted = false;
-	for (int pass = 1; (pass < N) && (!sorted); ++pass) {
-		sorted = true;
-		for (int cur = 0; cur < N - pass; ++cur) {
-			if (data[cur] > data[cur + 1]) {
-				swap(data[cur], data[cur + 1]);
-				sorted = false;
-			}
-		}
-	}
-}
--- pointer/pointer/sorting1.h
+++ pointer/pointer/sorting1.h
@@ -1,33 +1,36 @@
 #pragma once
 #include <cassert>
 #include <iostream>
-using namespace std;
+
 template <class T>
-class Sort
-{
-	T* m_data;
-	int m_size;
+class Sort {
+    T* m_data;
+    int m_size;
+
 public:
-	Sort(int Max = 100) : m_size(Max) {
-		m_data = new T[Max];
-	}
-	~Sort() {
-		if (m_data) delete[] m_data;
-	}
-	void initData(T* data, int size) {
-		m_size = size;
-		if (m_data)
-			delete[] m_data;
-		m_data = new T[m_size];
-		assert(m_data != nullptr);
-		memcpy(m_data, data, sizeof(T) * m_size);
-	}
-	int getSize() { return m_size; }
-	T* getData() { return m_data; }
-	void printAll() {
-		for (int i = 0; i < m_size; i++)
-			cout << m_data[i] << " ";
-		cout << endl;
-	}
-	virtual void sorting() = 0;
+    Sort(int Max = 100) : m_size(Max), m_data(new T[Max]) {}
+
+    ~Sort() {
+        if (m_data) delete[] m_data;
+    }
+
+    void initData(const T* data, int size) {
+        m_size = size;
+        if (m_data) delete[] m_data;
+        m_data = new T[m_size];
+        assert(m_data != nullptr);
+        std::copy(data, data + size, m_data);
+    }
+
+    int getSize() const { return m_size; }
+
+    const T* getData() const { return m_data; }
+
+    void printAll() const {
+        for (int i = 0; i < m_size; i++)
+            std::cout << m_data[i] << " ";
+        std::cout << std::endl;
+    }
+
+    virtual void sorting() = 0;
 };
