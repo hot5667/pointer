c9bc2770ee223ccbefa2dbbf28a1c0be5e1234bd
KimDoHyun1011 <hot99009@gmail.com>
2024-03-20 20:07:59 +0900

    템블릿 학습 시작

---
+++ pointer/pointer/CArrary.cpp
@@ -0,0 +1,14 @@
+#include "CArrary.h"
+
+#include <iostream>
+
+// 동적 배열 
+
+
+
+int main()
+{
+	CArrary arr;
+
+	return 0;
+}
--- pointer/pointer/class4.cpp
+++ pointer/pointer/class4.cpp
@@ -2,25 +2,77 @@
 
 using namespace std;
 
-class Marine
+//class Marine
+//{
+//public:
+//	void TakeDamage(int damage)
+//	{
+//		hp -= damage;
+//
+//		attack = 10;
+//	}
+//
+//	//특정 객체와 무관 
+//	// 전역 함수로 라고 생각하면 쉬움
+//	static void SetAttack(int value)
+//	{
+//
+//	}
+//
+//public:
+//	// 특정 객체와 연관
+//	int hp;
+//	static int attack; //특정 객체와 무관
+//};
+
+//class Player
+//{
+//public:
+//	Player()
+//	{
+//		id = s_idGenerator++;
+//	}
+//
+//public:
+//	int id;
+//	static int s_idGenerator;
+//};
+//
+//int GeneratorID()
+//{
+//	static int s_id = 1;
+//	return s_id++;
+//}
+
+//자료구조&알고리즘
+//디자인 패턴 
+// -싱글톤, 옵저버, 컴퍼넌트
+
+class UserManger
 {
 public:
-	void TakeDamage(int damage)
+	static UserManger* GetInstance()
 	{
-		hp -= damage;
+		static UserManger um;
+		return &um;
 	}
 
 public:
-	int hp;
-	int attack;
+	void AddUesr() { _userCount++; }
+	int GetUserCount() { return _userCount; }
+
+private:
+	int _userCount;
 };
 
+#define Get_Manger (UserManger::GetInstance())
 
 int main()
 {
-	Marine m1;
-	m1.hp = 20;
+	for (int i = 0; i < 10; ++i)
+	{
+		Get_Manger->AddUesr();
+	}
 
-	Marine m2;
-	m1.hp = 30;
+	cout << Get_Manger->GetUserCount() << endl;
 }
