f52eb5075db22dc00b833e827bd3bfb24e3f4242
KimDoHyun1011 <hot99009@gmail.com>
2024-03-23 23:33:16 +0900

    Item Drop 구현 시작

--- pointer/pointer/maintCArrary.cpp
+++ pointer/pointer/maintCArrary.cpp
@@ -12,15 +12,34 @@ public:
 };
 
 
+class Player
+{
+public:
+	Monster* _terget;
+};
+
+Player* FindPlayer(int id)
+{
+
+}
+
 int main()
 {
-	// malloc + free
-	// new + delete
+	// malloc + free C언어 
+	// new + delete C++ 언어 스타일
 
 	Monster* m1 = new Monster();
-
 	m1->_hp = 100;
+
+
+	Player* player = new Player();
+	player->_terget = m1;
 
 	delete m1;
 	//delete[] m1;
+
+	m1 = nullptr;
+
+	//use-After-Free
+	m1->_hp = 200;
 }
